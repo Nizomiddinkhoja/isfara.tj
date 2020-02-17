b3bdf05661f5da70641ea1ffedabf681c71949ac
Dilnoza <DilnozaIsoboeva@gmail.com>
2020-02-17 15:39:18 +0500

    Changed menu

--- include/navbar.php
+++ include/navbar.php
@@ -6,7 +6,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <!-- Brand -->
-        <a class="navbar-brand pt-0" href="./index.html">
+        <a class="navbar-brand pt-0" href="./index.php">
             <img src="./assets/img/brand/blue.png" class="navbar-brand-img" alt="...">
         </a>
         <!-- User -->
@@ -33,28 +33,20 @@
                 </a>
                 <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
                     <div class=" dropdown-header noti-title">
-                        <h6 class="text-overflow m-0">Welcome!</h6>
+                        <h6 class="text-overflow m-0">Добро пожаловать!</h6>
                     </div>
                     <a href="./examples/profile.html" class="dropdown-item">
                         <i class="ni ni-single-02"></i>
-                        <span>My profile</span>
+                        <span>Мой профиль</span>
                     </a>
                     <a href="./examples/profile.html" class="dropdown-item">
                         <i class="ni ni-settings-gear-65"></i>
-                        <span>Settings</span>
-                    </a>
-                    <a href="./examples/profile.html" class="dropdown-item">
-                        <i class="ni ni-calendar-grid-58"></i>
-                        <span>Activity</span>
-                    </a>
-                    <a href="./examples/profile.html" class="dropdown-item">
-                        <i class="ni ni-support-16"></i>
-                        <span>Support</span>
+                        <span>Настройки</span>
                     </a>
                     <div class="dropdown-divider"></div>
                     <a href="#!" class="dropdown-item">
                         <i class="ni ni-user-run"></i>
-                        <span>Logout</span>
+                        <span>Выход</span>
                     </a>
                 </div>
             </li>
@@ -65,7 +57,7 @@
             <div class="navbar-collapse-header d-md-none">
                 <div class="row">
                     <div class="col-6 collapse-brand">
-                        <a href="./index.html">
+                        <a href="./index.php">
                             <img src="./assets/img/brand/blue.png">
                         </a>
                     </div>
@@ -91,70 +83,88 @@
             <!-- Navigation -->
             <ul class="navbar-nav">
                 <li class="nav-item  active ">
-                    <a class="nav-link  active " href="./index.html">
-                        <i class="ni ni-tv-2 text-primary"></i> Dashboard
+                    <a class="nav-link  active " href="./index.php">
+                        <i class="ni ni-tv-2 text-primary"></i> Главная
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link " href="./examples/icons.html">
-                        <i class="ni ni-planet text-blue"></i> Icons
+                        <i class="ni ni-planet text-blue"></i> Новости
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link " href="./examples/maps.html">
-                        <i class="ni ni-pin-3 text-orange"></i> Maps
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link " href="./examples/profile.html">
-                        <i class="ni ni-single-02 text-yellow"></i> User profile
+                        <i class="ni ni-ungroup text-orange"></i> Категория
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link " href="./examples/tables.html">
-                        <i class="ni ni-bullet-list-67 text-red"></i> Tables
+                        <i class="ni ni-image text-blue"></i> Галерея
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="./examples/login.html">
-                        <i class="ni ni-key-25 text-info"></i> Login
+                        <i class="fa fa-question text-info"></i> Вопросы
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="./examples/register.html">
-                        <i class="ni ni-circle-08 text-pink"></i> Register
+                        <i class="ni ni-email-83 text-blue"></i> Электронная очередь
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./examples/register.html">
+                        <i class="ni ni-album-2 text-purple"></i> Слайдер
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./examples/register.html">
+                        <i class="fa fa-users text-orange"></i> Посетители
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./examples/register.html">
+                        <i class="ni ni-button-play text-red"></i> Видео
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./examples/register.html">
+                        <i class="ni ni-badge text-yellow"></i> Кто Что
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./examples/register.html">
+                        <i class="ni ni-key-25 text-black-50"></i> Администраторы
                     </a>
                 </li>
             </ul>
             <!-- Divider -->
-            <hr class="my-3">
-            <!-- Heading -->
-            <h6 class="navbar-heading text-muted">Documentation</h6>
-            <!-- Navigation -->
-            <ul class="navbar-nav mb-md-3">
-                <li class="nav-item">
-                    <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html">
-                        <i class="ni ni-spaceship"></i> Getting started
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html">
-                        <i class="ni ni-palette"></i> Foundation
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html">
-                        <i class="ni ni-ui-04"></i> Components
-                    </a>
-                </li>
-            </ul>
-            <ul class="navbar-nav">
-                <li class="nav-item active active-pro">
-                    <a class="nav-link" href="./examples/upgrade.html">
-                        <i class="ni ni-send text-dark"></i> Upgrade to PRO
-                    </a>
-                </li>
-            </ul>
+<!--            <hr class="my-3">-->
+<!--            <h6 class="navbar-heading text-muted">Documentation</h6>-->
+<!--            <ul class="navbar-nav mb-md-3">-->
+<!--                <li class="nav-item">-->
+<!--                    <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html">-->
+<!--                        <i class="ni ni-spaceship"></i> Getting started-->
+<!--                    </a>-->
+<!--                </li>-->
+<!--                <li class="nav-item">-->
+<!--                    <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html">-->
+<!--                        <i class="ni ni-palette"></i> Foundation-->
+<!--                    </a>-->
+<!--                </li>-->
+<!--                <li class="nav-item">-->
+<!--                    <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html">-->
+<!--                        <i class="ni ni-ui-04"></i> Components-->
+<!--                    </a>-->
+<!--                </li>-->
+<!--            </ul>-->
+<!--            <ul class="navbar-nav">-->
+<!--                <li class="nav-item active active-pro">-->
+<!--                    <a class="nav-link" href="./examples/upgrade.html">-->
+<!--                        <i class="ni ni-send text-dark"></i> Upgrade to PRO-->
+<!--                    </a>-->
+<!--                </li>-->
+<!--            </ul>-->
         </div>
     </div>
 </nav>
