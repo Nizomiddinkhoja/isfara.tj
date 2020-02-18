9abf3a017093c2f3266485ecf8d51f90bbe57ac3
nizomiddinkhoja <nizomiddinkhoja@gmail.com>
2020-02-18 13:11:53 +0500

    create add_news

--- login.php
+++ login.php
@@ -41,54 +41,7 @@
         <a class="navbar-brand" href="../index.html">
           <img src="../assets/img/brand/white.png" />
         </a>
-      <!--     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbar-collapse-main">
-          <!-- Collapse header --
-          <div class="navbar-collapse-header d-md-none">
-            <div class="row">
-              <div class="col-6 collapse-brand">
-                <a href="../index.html">
-                  <img src="../assets/img/brand/blue.png">
-                </a>
-              </div>
-            <div class="col-6 collapse-close">
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle sidenav">
-                  <span></span>
-                  <span></span>
-                </button>
-              </div>
-            </div>
-          </div>
-          <!-- Navbar items --
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link nav-link-icon" href="../index.html">
-                <i class="ni ni-planet"></i>
-                <span class="nav-link-inner--text">Dashboard</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link nav-link-icon" href="../examples/register.html">
-                <i class="ni ni-circle-08"></i>
-                <span class="nav-link-inner--text">Register</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link nav-link-icon" href="../examples/login.html">
-                <i class="ni ni-key-25"></i>
-                <span class="nav-link-inner--text">Login</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link nav-link-icon" href="../examples/profile.html">
-                <i class="ni ni-single-02"></i>
-                <span class="nav-link-inner--text">Profile</span>
-              </a>
-            </li>
-          </ul>
-        </div> -->
+     
       </div>
     </nav>
     <!-- Header -->
@@ -98,7 +51,6 @@
           <div class="row justify-content-center">
             <div class="col-lg-5 col-md-6">
               <h1 class="text-white">Добро пожаловать!</h1>
-              <!-- <p class="text-lead text-light">Use these awesome forms to login or create new account in your project for free.</p> -->
             </div>
           </div>
         </div>
@@ -114,24 +66,8 @@
       <div class="row justify-content-center">
         <div class="col-lg-5 col-md-7">
           <div class="card bg-secondary shadow border-0">
-<!--             <div class="card-header bg-transparent pb-5">
-              <div class="text-muted text-center mt-2 mb-3"><small>Sign in with</small></div>
-              <div class="btn-wrapper text-center">
-                <a href="#" class="btn btn-neutral btn-icon">
-                  <span class="btn-inner--icon"><img src="../assets/img/icons/common/github.svg"></span>
-                  <span class="btn-inner--text">Github</span>
-                </a>
-                <a href="#" class="btn btn-neutral btn-icon">
-                  <span class="btn-inner--icon"><img src="../assets/img/icons/common/google.svg"></span>
-                  <span class="btn-inner--text">Google</span>
-                </a>
-              </div>
-            </div> -->
-            <div class="card-body px-lg-5 py-lg-5">
+                        <div class="card-body px-lg-5 py-lg-5">
               <div class="text-muted text-center mt-2 mb-3"><small>Авторизация</small></div>
-              <!-- <div class="text-center text-muted mb-4">
-                <small>Or sign in with credentials</small>
-              </div> -->
               <form role="form">
                 <div class="form-group mb-3">
                   <div class="input-group input-group-alternative">
@@ -149,26 +85,12 @@
                     <input class="form-control" placeholder="Пароль" type="password">
                   </div>
                 </div>
-               <!--  <div class="custom-control custom-control-alternative custom-checkbox">
-                  <input class="custom-control-input" id=" customCheckLogin" type="checkbox">
-                  <label class="custom-control-label" for=" customCheckLogin">
-                    <span class="text-muted">Запомнить меня</span>
-                  </label>
-                </div> -->
                 <div class="text-center">
                   <button type="button" class="btn btn-primary my-4">Вход</button>
                 </div>
               </form>
             </div>
           </div>
-<!--           <div class="row mt-3">
-            <div class="col-6">
-              <a href="#" class="text-light"><small>Forgot password?</small></a>
-            </div>
-            <div class="col-6 text-right">
-              <a href="#" class="text-light"><small>Create new account</small></a>
-            </div>
-          </div> -->
         </div>
       </div>
     </div>
